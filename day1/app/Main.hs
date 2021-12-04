155a102bbffa01919824146dfb3e37aef33da74c
Christoph Ungricht <christoph.ungricht@outlook.com>
2021-12-04 21:15:58 +0100

    Day one finished

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -10,5 +10,6 @@ inputFileTest = "sonar-sweep-test.txt"
 
 main :: IO ()
 main = do
-  solution <- day1Part1 inputFile
-  print solution
+  solution1 <- day1Part1 inputFile
+  solution2 <- day1Part2 inputFile
+  putStr $ unlines ["Solution part 1: " ++ show solution1, "Solution part 1: " ++ show solution2]
--- day1/src/Day1.hs
+++ day1/src/Day1.hs
@@ -1,12 +1,13 @@
 module Day1
   ( day1Part1,
+    day1Part2,
   )
 where
 
 day1Part1 :: FilePath -> IO Int
 day1Part1 filePath = do
   content <- readFile filePath
-  return $ totalIncreases $ stringToInt $ lines content
+  return $ totalIncreases $ stringLinesToInt content
 
 totalIncreases :: [Int] -> Int
 totalIncreases (xs : ys : rest)
@@ -14,5 +15,17 @@ totalIncreases (xs : ys : rest)
   | ys <= xs = 0 + totalIncreases (ys : rest)
 totalIncreases _ = 0
 
+day1Part2 :: FilePath -> IO Int
+day1Part2 filePath = do
+  content <- readFile filePath
+  return $ totalIncreases . filterInts3 $ stringLinesToInt content
+
+stringLinesToInt :: String -> [Int]
+stringLinesToInt = stringToInt . lines
+
 stringToInt :: [String] -> [Int]
 stringToInt = map read
+
+filterInts3 :: [Int] -> [Int]
+filterInts3 (xs : ys : zs : rest) = (xs + ys + zs) : filterInts3 (ys : zs : rest)
+filterInts3 _ = []
