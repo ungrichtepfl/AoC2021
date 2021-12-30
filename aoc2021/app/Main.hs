b8123f48eb648b7082d986da7b921246cb1568ac
Christoph Ungricht <christoph.ungricht@outlook.com>
2021-12-30 12:45:48 +0100

    added possibility to only run 1 day

--- aoc2021/app/Main.hs
+++ aoc2021/app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 import Day1
@@ -5,6 +7,7 @@ import Day2
 import Day3
 import Day4
 import Day5
+import System.Environment
 
 -- Inputs
 
@@ -40,40 +43,45 @@ inputFileTestDay5 = "input/5_hydro-test.txt"
 
 -- Compute solutions to all problems:
 main :: IO ()
-main = do
-  -- Day 1
-  putStrLn "Start Day 1"
-  solD1P1 <- day1Part1 inputFileDay1
-  solD1P2 <- day1Part2 inputFileDay1
-  putStr $ unlines ["Solution Day 1 part 1:" ++ show solD1P1, "Solution Day 1 part 2: " ++ show solD1P2]
+main =
+  getArgs
+    >>= \case
+      ["1"] -> do
+        -- Day 1
+        putStrLn "Start Day 1"
+        solD1P1 <- day1Part1 inputFileDay1
+        putStrLn $ "Solution to Day 1 part 1 is: " ++ show solD1P1
+        solD1P2 <- day1Part2 inputFileDay1
+        putStrLn $ "Solution to Day 1 part 2 is: " ++ show solD1P2
+      ["2"] -> do
+        -- Day 2
+        putStrLn "Start Day 2"
+        solD2P1 <- day2Part1 inputFileDay2
+        putStrLn $ "Solution to Day 2 part 1 is: " ++ show solD2P1
+        solD2P2 <- day2Part2 inputFileDay2
+        putStrLn $ "Solution to Day 2 part 2 is: " ++ show solD2P2
+      ["3"] -> do
+        -- Day 3
+        putStrLn "Start Day 3"
+        solD3P1 <- day3Part1 inputFileDay3
+        putStrLn $ "Solution to Day 3 part 1 is: " ++ show solD3P1
 
-  -- Day 2
-  putStrLn "Start Day 2"
-  solD2P1 <- day2Part1 inputFileDay2
-  putStrLn $ "Solution to Day 2 part 1 is: " ++ show solD2P1
-  solD2P2 <- day2Part2 inputFileDay2
-  putStrLn $ "Solution to Day 2 part 2 is: " ++ show solD2P2
+        solD3P2 <- day3Part2 inputFileDay3
+        putStrLn $ "Solution to Day 3 part 2 is: " ++ show solD3P2
+      ["4"] -> do
+        -- Day 4
+        putStrLn "Start Day 4"
+        aolD4P1 <- day4Part1 inputFileDay4
+        putStrLn $ "Solution to Day 4 part 1 is: " ++ show aolD4P1
 
-  -- Day 3
-  putStrLn "Start Day 3"
-  solD3P1 <- day3Part1 inputFileDay3
-  putStrLn $ "Solution to Day 3 part 1 is: " ++ show solD3P1
+        aolD4P2 <- day4Part2 inputFileDay4
+        putStrLn $ "Solution to Day 4 part 2 is: " ++ show aolD4P2
+      ["5"] -> do
+        -- Day 5
+        putStrLn "Start Day 5"
+        aolD5P1 <- day5Part1 inputFileDay5
+        putStrLn $ "Solution to Day 5 part 1 is: " ++ show aolD5P1
 
-  solD3P2 <- day3Part2 inputFileDay3
-  putStrLn $ "Solution to Day 3 part 2 is: " ++ show solD3P2
-
-  -- Day 4
-  putStrLn "Start Day 4"
-  aolD4P1 <- day4Part1 inputFileDay4
-  putStrLn $ "Solution to Day 4 part 1 is: " ++ show aolD4P1
-
-  aolD4P2 <- day4Part2 inputFileDay4
-  putStrLn $ "Solution to Day 4 part 2 is: " ++ show aolD4P2
-
-  -- Day 5
-  putStrLn "Start Day 5"
-  aolD5P1 <- day5Part1 inputFileDay5
-  putStrLn $ "Solution to Day 5 part 1 is: " ++ show aolD5P1
-
-  aolD5P2 <- day5Part2 inputFileDay5
-  putStrLn $ "Solution to Day 5 part 2 is: " ++ show aolD5P2
+        aolD5P2 <- day5Part2 inputFileDay5
+        putStrLn $ "Solution to Day 5 part 2 is: " ++ show aolD5P2
+      _ -> putStrLn "Usage: aoc2021-solutions <DayNumber> "
