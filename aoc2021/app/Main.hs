b5e9e07f9f46c6237279dba3d5f2637978bb7aa7
Christoph Ungricht <christoph.ungricht@outlook.com>
2021-12-30 12:23:13 +0100

    Day 5 Part 1 and 2 finished

--- aoc2021/app/Main.hs
+++ aoc2021/app/Main.hs
@@ -42,17 +42,20 @@ inputFileTestDay5 = "input/5_hydro-test.txt"
 main :: IO ()
 main = do
   -- Day 1
+  putStrLn "Start Day 1"
   solD1P1 <- day1Part1 inputFileDay1
   solD1P2 <- day1Part2 inputFileDay1
   putStr $ unlines ["Solution Day 1 part 1:" ++ show solD1P1, "Solution Day 1 part 2: " ++ show solD1P2]
 
   -- Day 2
+  putStrLn "Start Day 2"
   solD2P1 <- day2Part1 inputFileDay2
   putStrLn $ "Solution to Day 2 part 1 is: " ++ show solD2P1
   solD2P2 <- day2Part2 inputFileDay2
   putStrLn $ "Solution to Day 2 part 2 is: " ++ show solD2P2
 
   -- Day 3
+  putStrLn "Start Day 3"
   solD3P1 <- day3Part1 inputFileDay3
   putStrLn $ "Solution to Day 3 part 1 is: " ++ show solD3P1
 
@@ -60,15 +63,17 @@ main = do
   putStrLn $ "Solution to Day 3 part 2 is: " ++ show solD3P2
 
   -- Day 4
+  putStrLn "Start Day 4"
   aolD4P1 <- day4Part1 inputFileDay4
   putStrLn $ "Solution to Day 4 part 1 is: " ++ show aolD4P1
 
   aolD4P2 <- day4Part2 inputFileDay4
   putStrLn $ "Solution to Day 4 part 2 is: " ++ show aolD4P2
-  
+
   -- Day 5
+  putStrLn "Start Day 5"
   aolD5P1 <- day5Part1 inputFileDay5
   putStrLn $ "Solution to Day 5 part 1 is: " ++ show aolD5P1
 
---  aolD5P2 <- day5Part2 inputFileDay5
---  putStrLn $ "Solution to Day 5 part 2 is: " ++ show aolD5P2
+  aolD5P2 <- day5Part2 inputFileDay5
+  putStrLn $ "Solution to Day 5 part 2 is: " ++ show aolD5P2
--- aoc2021/src/Day5.hs
+++ aoc2021/src/Day5.hs
@@ -4,9 +4,7 @@ module Day5
   )
 where
 
-import Data.List (foldl')
 import Data.List.Split (splitOn)
-import Data.Matrix
 
 --import qualified Debug.Trace as Dt
 
@@ -14,18 +12,26 @@ day5Part1 :: FilePath -> IO Int
 day5Part1 fp = do
   contents <- readFile fp
   let ls = parseLines $ lines contents :: [Line]
-  let finalFloor = markLines initFloor ls
-  putStrLn $ "Final floor:\n" ++ show finalFloor
+  let finalFloor = markLines1 initFloor ls
+  --  putStrLn $ "Final floor:\n" ++ show finalFloor
   return $ overlaps 2 finalFloor
 
 day5Part2 :: FilePath -> IO Int
-day5Part2 = undefined
+day5Part2 fp = do
+  contents <- readFile fp
+  let ls = parseLines $ lines contents :: [Line]
+  let finalFloor = markLines2 initFloor ls
+  --  putStrLn $ "Final floor:\n" ++ show finalFloor
+  return $ overlaps 2 finalFloor
 
 data OceanPoint = OceanPoint
   { position :: (Int, Int),
-    crossings :: Int
+    vents :: Int
   }
   deriving (Show)
+
+newOceanPoint :: (Int, Int) -> OceanPoint
+newOceanPoint pos = OceanPoint pos 1
 
 newtype OceanFloor = OceanFloor [OceanPoint] deriving (Show)
 
@@ -38,36 +44,52 @@ data Line = Line
 initFloor :: OceanFloor
 initFloor = OceanFloor []
 
-markLines :: OceanFloor -> [Line] -> OceanFloor
-markLines = foldl' markLine
+markLines1 :: OceanFloor -> [Line] -> OceanFloor
+markLines1 = foldr markLine1
 
-zeroOceanPoint :: (Int, Int) -> OceanPoint
-zeroOceanPoint pos = OceanPoint pos 0
+markLines2 :: OceanFloor -> [Line] -> OceanFloor
+markLines2 = foldr markLine2
 
-markLine :: OceanFloor -> Line -> OceanFloor
-markLine oceanFloor l = if null conns then oceanFloor else foldl' addToOceanFloor oceanFloor conns
-  where conns = connection l
+markLine1 :: Line -> OceanFloor -> OceanFloor
+markLine1 l oceanFloor = if null conns then oceanFloor else foldr addToOceanFloor oceanFloor conns
+  where
+    conns = connection1 l
 
-addToOceanFloor :: OceanFloor -> (Int, Int) -> OceanFloor
-addToOceanFloor (OceanFloor []) pos = OceanFloor [zeroOceanPoint pos]
-addToOceanFloor (OceanFloor ops) pos =
+markLine2 :: Line -> OceanFloor -> OceanFloor
+markLine2 l oceanFloor = if null conns then oceanFloor else foldr addToOceanFloor oceanFloor conns
+  where
+    conns = connection2 l
+
+addToOceanFloor :: (Int, Int) -> OceanFloor -> OceanFloor
+addToOceanFloor pos (OceanFloor []) = OceanFloor [newOceanPoint pos]
+addToOceanFloor pos (OceanFloor ops) =
   if any (posContained pos) ops
     then OceanFloor $ addCrossing ops pos
-    else OceanFloor $ zeroOceanPoint pos : ops
-  where
-    addCrossing :: [OceanPoint] -> (Int, Int) -> [OceanPoint]
-    addCrossing [] _ = []
-    addCrossing (fop@(OceanPoint pos1 n) : ops') pos2
-      | pos1 == pos2 = OceanPoint pos1 (n + 1) : addCrossing ops' pos2
-      | otherwise = fop : addCrossing ops' pos2
+    else OceanFloor $ newOceanPoint pos : ops
+
+addCrossing :: [OceanPoint] -> (Int, Int) -> [OceanPoint]
+addCrossing [] _ = []
+addCrossing (fop@(OceanPoint pos1 n) : ops') pos2
+  | pos1 == pos2 = OceanPoint pos1 (n + 1) : addCrossing ops' pos2
+  | otherwise = fop : addCrossing ops' pos2
 
 posContained :: (Int, Int) -> OceanPoint -> Bool
 posContained p2 (OceanPoint p1 _) = p1 == p2
 
-connection :: Line -> [(Int, Int)]
-connection (Line (x1, y1) (x2, y2))
+-- | without diagonals
+connection1 :: Line -> [(Int, Int)]
+connection1 (Line (x1, y1) (x2, y2))
   | x1 == x2 = zip (replicate (abs (y1 - y2) + 1) x1) (if y2 > y1 then [y1 .. y2] else [y2 .. y1])
   | y1 == y2 = zip (if x2 > x1 then [x1 .. x2] else [x2 .. x1]) (replicate (abs (x1 - x2) + 1) y1)
+  | otherwise = []
+
+-- | with diagonals
+connection2 :: Line -> [(Int, Int)]
+connection2 (Line (x1, y1) (x2, y2))
+  | x1 == x2 = zip (replicate (abs (y1 - y2) + 1) x1) (if y2 > y1 then [y1 .. y2] else [y2 .. y1])
+  | y1 == y2 = zip (if x2 > x1 then [x1 .. x2] else [x2 .. x1]) (replicate (abs (x1 - x2) + 1) y1)
+  | abs (y2 - y1) == abs (x2 - x1) =
+    zip (if x2 >= x1 then [x1 .. x2] else [x1, (x1 -1) .. x2]) (if y2 >= y1 then [y1 .. y2] else [y1, (y1 -1) .. y2])
   | otherwise = []
 
 parseLines :: [String] -> [Line]
@@ -76,7 +98,7 @@ parseLines = map str2line
     str2line :: String -> Line
     str2line str =
       let [[x1, y1], [x2, y2]] = map (map read . splitOn ",") (splitOn " -> " str) :: [[Int]]
-       in Line (y1, x1) (y2, x2) -- x is column and y is row
+       in Line (x1, y1) (x2, y2) -- x is column and y is row
 
 overlaps :: Int -> OceanFloor -> Int
 overlaps n (OceanFloor ops) = length $ filter (\(OceanPoint _ n') -> n' >= n) ops
