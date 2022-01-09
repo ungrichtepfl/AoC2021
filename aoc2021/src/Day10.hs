a454682ec33435bb778fbe30855602ca29a6792e
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-01-09 15:15:43 +0100

    use do notation for Either

--- aoc2021/src/Day10.hs
+++ aoc2021/src/Day10.hs
@@ -9,7 +9,7 @@ day10Part1 fp = do
   contents <- readFile fp
   let chunks = parseInputs contents
   let checkedChunks = map (checkChunk []) chunks
-  --  printErrors checkedChunks
+  printErrors $ zip chunks checkedChunks
   return $ calculateScore checkedChunks
 
 day10Part2 :: FilePath -> IO Int
@@ -49,18 +49,18 @@ calculateScore ((Left (_, found)) : rest) = case found of
   '>' -> 25137 + calculateScore rest
   _ -> error $ "Wrong kind of bracket: " ++ show found
 
-printErrors :: [Either (Bracket, Bracket) Stack] -> IO ()
+printErrors :: [(Chunk, Either (Bracket, Bracket) Stack)] -> IO ()
 printErrors [] = return ()
-printErrors ((Left (expected, found)) : rest) = putStrLn ("Expected " ++ show expected ++ ", but found " ++ show found ++ " instead.") >> printErrors rest
-printErrors ((Right _) : rest) = printErrors rest
+printErrors ((c, Left (expected, found)) : rest) = putStrLn (show c ++ " - Expected " ++ show expected ++ ", but found " ++ show found ++ " instead.") >> printErrors rest
+printErrors (_ : rest) = printErrors rest
 
 checkChunk :: Stack -> Chunk -> Either (Bracket, Bracket) Stack
 checkChunk s [] = Right s
 checkChunk s (b : bs)
   | b `elem` open = checkChunk (push s b) bs
-  | b `elem` closed = case pop s b of
-    Right ns -> checkChunk ns bs
-    ef -> ef
+  | b `elem` closed = do
+    ns <- pop s b
+    checkChunk ns bs
   | otherwise = error $ "Wrong kind of bracket. " ++ show b
 
 pop :: Stack -> Bracket -> Either (Bracket, Bracket) Stack
