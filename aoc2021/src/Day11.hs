e62555fe75878b1b098f0e6637eaeaafb8aa612c
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-01-16 20:39:07 +0100

    bugfix

--- aoc2021/src/Day11.hs
+++ aoc2021/src/Day11.hs
@@ -20,22 +20,22 @@ type Energy = Int
 type Octopuses = Matrix Energy
 
 increaseBy1 :: Octopuses -> Octopuses
-increaseBy1 oct = elementwiseUnsafe (+) oct (fromList (nrows oct) (ncols oct) (replicate (nrows oct * ncols oct) 1))
+increaseBy1 = mapPos (\_ e -> if e == 9 then 0 else e + 1)
 
 initialFlashing :: Octopuses -> [(Int, Int)]
-initialFlashing = elemIndicesMat 10 . increaseBy1
+initialFlashing = elemIndicesMat 0
 
 updateEnergy :: Octopuses -> Octopuses
-updateEnergy oct = foldr updateAround oct $ initialFlashing oct
+updateEnergy oct = let octUpdated = increaseBy1 oct in foldr updateAround octUpdated $ initialFlashing octUpdated
 
 updateAround :: (Int, Int) -> Octopuses -> Octopuses
-updateAround (i, j) oct = foldr go oct [(i + 1, j), (i - 1, j), (i, j + 1), (i + 1, j), (i + 1, j + 1), (i -1, j -1), (i + 1, j -1), (i -1, j + 1)]
+updateAround (i, j) oct = foldr go oct [(i + 1, j), (i - 1, j), (i, j + 1), (i, j -1), (i + 1, j + 1), (i -1, j -1), (i + 1, j -1), (i -1, j + 1)]
   where
     go :: (Int, Int) -> Octopuses -> Octopuses
     go idx@(i', j') accumOct
       | i' < 1 || i' > nrows accumOct || j' < 1 || j' > ncols accumOct = accumOct
-      | accumOct ! idx == 10 = accumOct
-      | accumOct ! idx == 9 = updateAround idx $ setElem 10 idx accumOct
+      | accumOct ! idx == 0 = accumOct
+      | accumOct ! idx == 9 = updateAround idx $ setElem 0 idx accumOct
       | otherwise = setElem (accumOct ! idx + 1) idx accumOct
 
 countFlashing :: Octopuses -> [(Int, Int)] -> Int
